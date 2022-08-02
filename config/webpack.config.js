e08b60145b8724333a4f8fe028df443403ecc49c
chenhao <chenhao@icourt.cc>
2022-08-02 10:09:52 +0800

    webpack 路径修改

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -85,6 +85,8 @@ const hasJsxRuntime = (() => {
   }
 })();
 
+// 打包输出资源统一文件夹 
+const resourcePath = 'resource-static-tool'
 // This is the production and development configuration.
 // It is focused on developer experience, fast rebuilds, and a minimal bundle.
 module.exports = function (webpackEnv) {
@@ -209,13 +211,13 @@ module.exports = function (webpackEnv) {
       // There will be one main bundle, and one file per asynchronous chunk.
       // In development, it does not produce real files.
       filename: isEnvProduction
-        ? 'static/js/[name].[contenthash:8].js'
-        : isEnvDevelopment && 'static/js/bundle.js',
+        ? `${resourcePath}/js/[name].[contenthash:8].js`
+        : isEnvDevelopment && `${resourcePath}/js/bundle.js`,
       // There are also additional JS chunk files if you use code splitting.
       chunkFilename: isEnvProduction
-        ? 'static/js/[name].[contenthash:8].chunk.js'
-        : isEnvDevelopment && 'static/js/[name].chunk.js',
-      assetModuleFilename: 'static/media/[name].[hash][ext]',
+        ? `${resourcePath}/js/[name].[contenthash:8].chunk.js`
+        : isEnvDevelopment && `${resourcePath}/js/[name].chunk.js`,
+      assetModuleFilename: `${resourcePath}/media/[name].[hash][ext]`,
       // webpack uses `publicPath` to determine where the app is being served from.
       // It requires a trailing slash, or the file assets will get an incorrect path.
       // We inferred the "public path" (such as / or /my-project) from homepage.
@@ -393,7 +395,7 @@ module.exports = function (webpackEnv) {
                 {
                   loader: require.resolve('file-loader'),
                   options: {
-                    name: 'static/media/[name].[hash].[ext]',
+                    name: `${resourcePath}/media/[name].[hash].[ext]`,
                   },
                 },
               ],
@@ -625,8 +627,8 @@ module.exports = function (webpackEnv) {
         new MiniCssExtractPlugin({
           // Options similar to the same options in webpackOptions.output
           // both options are optional
-          filename: 'static/css/[name].[contenthash:8].css',
-          chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
+          filename: `${resourcePath}/css/[name].[contenthash:8].css`,
+          chunkFilename: `${resourcePath}/css/[name].[contenthash:8].chunk.css`,
         }),
       // Generate an asset manifest file with the following content:
       // - "files" key: Mapping of all asset filenames to their corresponding
